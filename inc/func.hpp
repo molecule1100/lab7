bf1887c62bae9c5d75dce7ca5329265d522ea509
molecule1100 <damnfker@gmail.com>
2022-11-30 19:15:32 +0200

    added func.hpp

---
+++ inc/func.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+void addMat(int mat[100][100], int columns, int rows);
+
+bool isZeroSum(int mat[100][100], int columns, int rows);
+
+bool isPrime(int N);
+
+void sortMat(int mat[100][100], int columns, int rows);
+
+void outputMat(int mat[100][100], int columns, int rows);
